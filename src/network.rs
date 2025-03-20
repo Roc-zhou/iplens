93e551c7157825b046b438ceec47bdeb6f7a6f0a
@AB <1137938565@qq.com>
2025-03-20 23:50:17 +0800

    change

--- src/network.rs
+++ src/network.rs
@@ -4,31 +4,48 @@ use serde_json::Value;
 use std::error::Error;
 use std::net::IpAddr;
 
-/// 获取本地 IP 地址（过滤回环地址）
+/// 获取本地 IP 地址（只返回主要的 IPv4 地址）
 pub fn get_local_ips() -> Result<Vec<(String, IpAddr)>, Box<dyn Error>> {
     let mut ips = Vec::new();
 
+    // 需要排除的接口名称
+    let excluded_interfaces = [
+        "lo",
+        "docker",
+        "veth",
+        "br-",
+        "vmnet",
+        "virtual",
+        "VirtualBox",
+        "virbr",
+        "Loopback",
+        "VMware",
+    ];
+
     for interface in datalink::interfaces() {
-        let interface_name = interface.name.clone();
+        let interface_name = interface.name.clone().to_lowercase();
+
+        // 跳过不需要的接口
+        if excluded_interfaces
+            .iter()
+            .any(|&x| interface_name.contains(x))
+        {
+            continue;
+        }
+
+        // 只获取 IPv4 地址
         for ip in interface.ips {
-            match ip.ip() {
-                IpAddr::V4(ipv4) if !ipv4.is_loopback() => {
-                    ips.push((interface_name.clone(), IpAddr::V4(ipv4)));
+            if let IpAddr::V4(ipv4) = ip.ip() {
+                if !ipv4.is_loopback() && !ipv4.is_link_local() && !ipv4.is_unspecified() {
+                    ips.push((interface.name.clone(), IpAddr::V4(ipv4)));
+                    break; // 每个接口只取第一个有效的 IPv4 地址
                 }
-                IpAddr::V6(ipv6) if !ipv6.is_loopback() => {
-                    ips.push((interface_name.clone(), IpAddr::V6(ipv6)));
-                }
-                _ => {}
             }
         }
     }
 
-    // 优先排序 IPv4 地址
-    ips.sort_by(|a, b| match (a.1, b.1) {
-        (IpAddr::V4(_), IpAddr::V6(_)) => std::cmp::Ordering::Less,
-        (IpAddr::V6(_), IpAddr::V4(_)) => std::cmp::Ordering::Greater,
-        _ => a.0.cmp(&b.0),
-    });
+    // 按接口名称排序
+    ips.sort_by(|a, b| a.0.cmp(&b.0));
 
     Ok(ips)
 }
